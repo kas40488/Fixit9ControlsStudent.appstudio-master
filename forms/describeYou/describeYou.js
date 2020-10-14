e9188d4a54ea2653527d370d3cde49ba94aedb58
kas40488 <kas40488@creighton.edu>
2020-10-14 15:17:07 -0500

    Changes

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,9 @@
 
+rdoCharacteristics.onclick=function(){
+  let userChoice = $("input[name=rdoCharacteristics]:checked").prop("value")
+  lblMessage.value = `I would agree that you are a ${userChoice} person too.`
+}
 
-
-
-
+Button1.onclick=function(){
+  ChangeForm(favExercises)
+}
--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -1,22 +1,24 @@
 var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
-                  'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
+                  'Keylime Pie','Cherry Cobbler','Chocolate Chip Cookies']
 
 dessertVoting.onshow=function(){
     drpDesserts.clear()   
-  /* add a loop here that adds all the desserts in the array 
-     to the dropdown.
-  */
+  for (i = 0; i < dessertList.length; i++)
+    drpDesserts.addItem(dessertList[i])
 }
 
 
-drpDesserts.onclick=function(){
+drpDesserts.onclick=function(choice){
    // this 'if' kicks user out if they  just clicked on control 
  // but not on one item in the list.
-    if (typeof(s) == "object")   
+    if (typeof(choice) == "object")   
       return                    
     else {  // the user picked something
-        /* add code here to get item user chose and output 
-        it in a label, using a literal.
-        */
+      drpDesserts.value = choice
+      lblPrompt.value = (`The user chose ${choice} and .selection is ${drpDesserts.selection}.`)
     }
+}
+
+btnNextPage.onclick=function(){
+  ChangeForm(describeYou)
 }
