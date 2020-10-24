e5ecb88113a13ddbe86e4d83f6b2701f1f6efafa
kas40488 <kas40488@creighton.edu>
2020-10-24 13:30:16 -0500

    Updates

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -5,27 +5,38 @@ favExercises.onshow=function(){
     selExercises.clear()   
   for (i = 0; i < exercises.length; i++)
     selExercises.addItem(exercises[i])
-  for (i = 0; i < coreExercises.length; i++)
-    selExercises.addItem(coreExercises[i])
 }
 
-btnSubmit.onclick=function(choice){
-  if (typeof(choice) == "object")
-    return
-  else if (choice.length == 2) {
-    for (i = 0; i < 2; i++)
-      choice.includes(coreExercises)
-    lblMsg.value = (`You chose situps and planks - those are the two core exercises.`)
+btnSubmit.onclick=function(){
+  if (selExercises.text[0] == "situps" && selExercises.text[1] == "plank") {
+      lblMsg2.value = `You chose ${selExercises.text[0]} and ${selExercises.text[1]} - those are the two core exercises.`
 }  else {
-    lblMsg.value = (`You did not pick the two core exercises.`)
-  }
+      lblMsg2.value = "You did not pick the two core exercises."
+    }
 }
-
 
 btnChangeMobileNav.onclick=function(){
   ChangeForm(mobileNav)
 }
 
+
+/*
+  for (i = 0; i < coreExercises.length; i++)
+    selExercises.addItem(coreExercises[i])
+    
+    
+      
+  else if (choice.length == 2) {
+    for (i = 0; i < 2; i++)
+      choice.includes(coreExercises)
+
+}  else {
+
+  }
+}
+
+  if (typeof(choice) == "object")
+    return
 if (coreExercises.includes(choice[0]))
 # boolean for second one
-
+    */
